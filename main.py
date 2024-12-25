5fa29f3415925b75bc873570b5ad73f9b2ed96c9
Digipr01 <tychokruithof2008@hotmail.com>
2024-12-25 17:02:51 +0100

    Added Terminate and info commands, be careful as anyone can use these! Also changed some print statements

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import discord
 from discord import app_commands
+import os
 import json
 
 def parseToken():
     with open("configure.json") as read_file:
         file_data = json.load(read_file)
-        print(type(file_data))
         return file_data["token"]
 
 intents = discord.Intents.default()
@@ -14,12 +14,21 @@ tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
-    print(f"Bot starting as {client.user.name}.")
+    print(f"Bot online as {client.user.name}.")
     await tree.sync()
+    print("synced commands")
 
 @tree.command(name="hi", description="This is really just a test")
 async def command_hi(interaction):
     await interaction.response.send_message("DannyHPb")
-    
+
+@tree.command(name="info", description="About the bot")
+async def command_info(interaction):
+	await interaction.response.send_message("This bot was made to automate daily scrambles in the DannyHTv discord server. It was made and hosted by Digipr01. For any questions and suggestions, you can DM him.")
+
+@tree.command(name="terminate", description="Shuts off the bot")
+async def command_shutdown(interaction):
+	await interaction.response.send_message("Shutting down DannyHTV bot")
+	os._exit(os.EX_OK)
 token = parseToken()
 client.run(token)
